2885d11310de1dac5a5d27609da16f1f0ae264d6
Gaurav Tewari <gauravtewari111@gmail.com>
2020-09-30 23:10:46 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'questoins.dart';
+import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +30,61 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> socrekeeper = [];
+  // List<String> questions = [
+  //   'You can lead a cow down stairs but not up stairs.',
+  //   'Approximately one quarter of human bones are in the feet.',
+  //   'A slug\'s blood is green.',
+  // ];
+  // List<bool> answers = [
+  //   false,
+  //   true,
+  //   true,
+  // ];
+  // Question q1 = Question(
+  //     q: 'You can lead a cow down stairs but not up stairs.', a: false);
+
+  int questionNumber = 0;
+  void checkAnswer(bool userAnswer) {
+    print(quizBrain.getCurrent());
+
+    // print(questionNumber);
+    // print(quizBrain.getLast());
+    if (quizBrain.getCurrent() == quizBrain.getLast() - 1) {
+      Alert(context: context, title: "Over", desc: "Quiz is over.").show();
+      quizBrain.setZero();
+      // print('over');
+      socrekeeper = [];
+    }
+
+    bool correctAnswer = quizBrain.getQuestionAnswer(questionNumber);
+    if (correctAnswer == userAnswer) {
+      // print('user got it');
+      setState(() {
+        quizBrain.nextQuestion();
+
+        socrekeeper.add(
+          Icon(
+            Icons.check,
+            color: Colors.green,
+          ),
+        );
+      });
+    } else {
+      // print('pagal hah sala');
+      setState(() {
+        quizBrain.nextQuestion();
+
+        socrekeeper.add(
+          Icon(
+            Icons.close,
+            color: Colors.red,
+          ),
+        );
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +97,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(questionNumber),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +122,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                checkAnswer(true);
               },
             ),
           ),
@@ -80,11 +141,15 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                checkAnswer(false);
               },
             ),
           ),
         ),
         //TODO: Add a Row here as your score keeper
+        Row(
+          children: socrekeeper,
+        ),
       ],
     );
   }
